5dcb9583fdf6b24a055977c7bb357f7c24a3fd9b
horomi-chan <herozaki.horomi@gmail.com>
2022-07-31 01:49:02 +0900

    Get first-post

--- slack-my-app/lib/spreadsheet.js
+++ slack-my-app/lib/spreadsheet.js
@@ -35,7 +35,7 @@ export const getChannels = async () => {
   return [];
 };
 
-// チャンネル名を変数にしてそこから内容を取得する
+//仮で末尾10件取得して画面遷移を早くさせる
 export const getContentByChannel = async (channel) => {
   const sheets = getSheets();
   const response = await sheets.spreadsheets.values.get({
@@ -44,8 +44,9 @@ export const getContentByChannel = async (channel) => {
   });
   const rows = response.data.values;
   if (rows) {
-    return rows.slice(-10).map((row) => {
+    return rows.slice(-10).map((row, id) => {
       return {
+        id: id + 1,
         date: row[0],
         name: row[1],
         post: row[2],
@@ -54,3 +55,50 @@ export const getContentByChannel = async (channel) => {
   }
   return [];
 };
+//まずはじめの投稿を何かを取得する作戦
+// はじめの投稿と取得した10件ターンとで重複があったらreadmoreボタンを非表示にする
+export const getFirstPost = async (channel, pageSize = 1) => {
+  const sheets = getSheets();
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    range: channel,
+  });
+  const rows = response.data.values;
+  if (rows) {
+    return rows.slice(0, pageSize).map((row) => {
+      return {
+        date: row[0],
+        name: row[1],
+        post: row[2],
+      };
+    });
+  }
+
+  return [];
+};
+
+// 10ずつ取得できる数式を入れたい.....
+// useEffectで回数をカウントする？
+// -10,-1 → -20,-11 → -30,-21....-10*n,-1-10*(n-1)
+// もし残りのはじめの投稿を含んでいなかったら次へ進む
+// 仮：今は残り全部を取得するようにしておく
+// export const getReadmoreContent = async (channel) => {
+//   const sheets = getSheets();
+//   const response = await sheets.spreadsheets.values.get({
+//     spreadsheetId: process.env.SPREADSHEET_ID,
+//     range: channel,
+//   });
+//   const rows = response.data.values;
+
+//   if (rows) {
+//     return rows.slice(0, -11).map((row, id) => {
+//       return {
+//         id: id + 1,
+//         date: row[0],
+//         name: row[1],
+//         post: row[2],
+//       };
+//     });
+//   }
+//   return [];
+// };
