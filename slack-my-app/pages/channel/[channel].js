7d62950eaf6098e9073d85a7c037c0394d0423d1
horomi-chan <herozaki.horomi@gmail.com>
2022-07-31 01:50:23 +0900

    Display first-post_verify

--- slack-my-app/pages/channel/[channel].js
+++ slack-my-app/pages/channel/[channel].js
@@ -1,4 +1,9 @@
-import { getChannels, getContentByChannel } from "../../lib/spreadsheet";
+import {
+  getChannels,
+  getContentByChannel,
+  // getReadmoreContent,
+  getFirstPost,
+} from "../../lib/spreadsheet";
 import styles from "../../styles/Home.module.css";
 import Head from "next/head";
 import Link from "next/link";
@@ -14,8 +19,10 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params: { channel } }) {
+export async function getStaticProps({ params: { channel, pageSize } }) {
   const content = await getContentByChannel(channel);
+  // const readmore = await getReadmoreContent(channel);
+  const first = await getFirstPost(channel, pageSize);
 
   if (!content) {
     console.log(`Failed to find post for channel: ${channel}`);
@@ -33,15 +40,18 @@ export async function getStaticProps({ params: { channel } }) {
     props: {
       content,
       channels,
+      // readmore,
+      first,
     },
     revalidate: 60,
   };
 }
-export default function RenderContent({ content, channels }) {
+export default function RenderContent({ content, channels, first }) {
   const router = useRouter();
   const activeName = decodeURI(router.asPath).substring(
     decodeURI(router.asPath).lastIndexOf("/") + 1
   );
+
   return (
     <div className={styles.container}>
       <Head>
@@ -78,7 +88,9 @@ export default function RenderContent({ content, channels }) {
             <div className={styles.header}>{activeName}</div>
             <div className={styles.scroll}>
               <div>
-                {content.map((post) => {
+                <p>はじめの投稿</p>
+                {console.log(first)}
+                {first.map((post) => {
                   return (
                     <div className={styles.post}>
                       <div className={styles.textcols}>
@@ -89,6 +101,26 @@ export default function RenderContent({ content, channels }) {
                     </div>
                   );
                 })}
+
+                {/* 先頭を含まないターンでは戻るボタンを押したら続きが10件追加されるようにしたい */}
+                {/* {content.length <= 10 ? null : <button>readmore!!!</button>} */}
+                {console.log(content.length)}
+                {/* 裏で既に全件数取得できていないと10件以上続きがあるかどうかは不明のまま */}
+                {<button type="button">Read more!!</button>}
+
+                {content.map((post) => {
+                  return (
+                    <div className={styles.post}>
+                      <p>{post.id}</p>
+                      <div className={styles.textcols}>
+                        <p>{post.name}</p>
+                        <p>{post.date}</p>
+                      </div>
+                      <p className={styles.textContent}>{post.post}</p>
+                    </div>
+                  );
+                })}
+                {/* 末尾を含まないターンでは次ボタンを押されたら10件追加したい */}
                 {console.log("GET!!!____content")}
               </div>
             </div>
